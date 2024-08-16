11f9e2bd0a1006d5bc8145e28e920e0b66abd707
jacqueline kenny <jkennyvasconcelos@hotmail.com>
2024-08-16 03:27:12 -0300

    salvando alteracoes

--- src/components/Cabecalho/styles.ts
+++ src/components/Cabecalho/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
   background-color: ${(props) => props.theme.corSecundaria};
-  color: ${(props) => props.theme.corPrincipal};
+  color: ${(props) => props.theme.corPrincipal}
   text-align: center;
   padding: 24px 0;
+  `
